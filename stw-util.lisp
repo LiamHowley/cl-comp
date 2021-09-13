4adb28ea3287bf65633abb24961ac52438cbd196
Liam Howley <liam.howley@hotmail.com>
2021-09-13 10:12:18 +0100

    helper macros for defining and accessing special contexts

--- stw-util.lisp
+++ stw-util.lisp
@@ -27,8 +27,8 @@
   "Find all superclasses in the inheritance hierarchy 
 of a class. Results are cached unless nil."
   (let ((class (class-definition class)))
-    (when (class-finalized-p class)
-      (cache-class-precedents class))))
+;;    (when (class-finalized-p class)
+      (cache-class-precedents class)))
 
 (define-memo-function (cache-class-precedents :table *class-precedents*) (class)
   (class-precedents% class))
@@ -63,17 +63,28 @@ of object-type associated with a class, including
 inherited slots. Recursively walks through superclasses. 
 Results are cached unless nil."
   (let ((class (class-definition class)))
-    (when (class-finalized-p class)
-      (cache-filter-slots-by-type class object-type))))
+;;    (when (class-finalized-p class)
+      (cache-filter-slots-by-type class object-type)))
   
 
+(defun filtered-effective-slots (class filter)
+  (loop for slot in (class-slots class)
+     for target-slotp = (funcall filter slot)
+     if target-slotp
+     collect slot))
+
+
 (define-memo-function (cache-filter-slots-by-type :table *filtered-slots*) (class object-type)
-  (let ((object-type (if (and (symbolp object-type)
-			      (find-class 'object-type nil))
-			 (find-class 'object-type)
-			 object-type)))
-    (filtered-slots class #'(lambda (slot)
-			      (typep slot object-type)))))
+  (let* ((object-type (if (and (symbolp object-type)
+			       (find-class 'object-type nil))
+			  (find-class 'object-type)
+			  object-type))
+	 (predicate #'(lambda (slot)
+			(typep slot object-type)))
+	 (slots (when (class-finalized-p (class-definition class))
+		  (filtered-effective-slots (class-definition class) predicate))))
+    (if slots slots
+	(filtered-slots (class-definition class) predicate))))
 
 
 
@@ -85,10 +96,73 @@ Results are cached unless nil."
   "Return and cache slot definition of slot-name and type.
 Results are cached unless nil."
   (let ((class (class-definition class)))
-    (when (class-finalized-p class)
-      (cache-find-slot-definition class slot-name type))))
+;;    (when (class-finalized-p class)
+      (cache-find-slot-definition class slot-name type)))
 
 (define-memo-function (cache-find-slot-definition :table *slot-definitions*) (class slot-name type)
-  (loop for slot in (filter-slots-by-type (class-definition class) type)
-     when (eq (slot-definition-name slot) slot-name)
-     return slot))
+  (let ((slot (when (class-finalized-p (class-definition class))
+		(setf slot (loop for slot in (class-slots (class-definition class))
+			      when (and (eq (slot-definition-name slot) slot-name)
+					(typep slot type))
+			      return slot)))))
+    (if slot slot
+	(loop for slot in (filter-slots-by-type (class-definition class) type)
+	   when (eq (slot-definition-name slot) slot-name)
+	   return slot))))
+
+
+
+
+;;; defining context for use with special slots
+
+(defmacro define-class-context (env class-type active-layers (&rest rest &key &allow-other-keys)
+				  &body body)
+  "Sets and captures the dynamic environment for specific contexts. The symbol set with the 
+ENV variable is returned along with the captured environment. This encapsulates active layers 
+FORM-LAYER and dynamic values of class slot accessors, set through keyword args. 
+To be used in conjunction with the WITH-OBJECT-IN-CONTEXT macro."
+  (let ((slot (gensym))
+	(value (gensym)))
+    `(symbol-macrolet ((class-definition ,(find-class class-type)))
+       (macrolet ((get-value (,slot ,value)
+		    `(cond (,,value ,,value)
+			   ((slot-value ,class-definition ',,slot)
+			    (slot-value ,class-definition ',,slot))
+			   (t nil))))
+	 (progn
+	   (defdynamic ,env
+	     (dynamic-let ((class-type ',class-type))
+	       (with-active-layers ,active-layers
+		 (dletf (,@(loop for (key value) on rest by #'cddr
+			      for slot = (find-symbol (symbol-name key))
+			      collect `((,slot class-definition) (get-value ,slot ,value))))
+		   ,@body
+		   (capture-dynamic-environment))))))))))
+
+
+(defmacro with-class-in-context (env class-instance &body body)
+  "Facilitates access to the dynamic context encapsulated by ENV. 
+ENV is set by the DEFINE-CONTEXT macro."
+  `(with-dynamic-environment ((dynamic ,env))
+     (unless (typep ,class-instance (dynamic class-type))
+       (error "the object ~a is not of type ~a. Out of context." ,class-instance (dynamic class-type)))
+     (let ((the-class ,class-instance))
+       ,@body)))
+
+
+
+(defmacro define-layer-context (env active-layers bindings &body body)
+  "Sets and captures the dynamic environment for specific contexts and bindings. The symbol set with the 
+ENV variable is returned along with the captured environment."
+  `(defdynamic ,env
+     (with-active-layers ,active-layers
+       (dletf ,bindings
+	 ,@body
+	 (capture-dynamic-environment)))))
+
+
+(defmacro with-context (env &body body)
+  "Facilitates access to the dynamic context encapsulated by ENV. 
+ENV is set by the DEFINE-LAYERED-CONTEXT macro."
+  `(with-dynamic-environment ((dynamic ,env))
+     ,@body))
