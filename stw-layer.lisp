41ac53ec28c1015c4604b4eb17618bb4ca633c4c
Liam Howley <liam.howley@hotmail.com>
2022-06-12 23:15:58 +0100

    using adjoin-layer-using-class to create default setting of exclusive domains between layers of type stw-base-layer

--- stw-layer.lisp
+++ stw-layer.lisp
@@ -9,3 +9,13 @@
 (deflayer stw-base-layer ()
   ()
   (:metaclass stw-layer-context))
+
+
+(define-layered-method adjoin-layer-using-class
+  ((layer stw-layer-context) active-layers)
+  ;; on layer activation deactivate other layers of the same layer type
+  (values 
+   (call-next-layered-method
+    layer
+    (remove-layer 'stw-base-layer active-layers))
+   t))
